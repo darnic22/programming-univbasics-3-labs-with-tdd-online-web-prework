6fd2dd9eac4f157b3fd2be28f0a02d3abc607c8d
Nicole M Darby <nicoledarby021322@gmail.com>
2019-09-21 05:43:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,7 @@ second_number=5
 sum=first_number+second_number
 difference=first_number-second_number
 product=first_number*second_number
+<<<<<<< HEAD
 quotient=first_number/second_number
+=======
+>>>>>>> dc3ff5e32011572a8337b6f564b3169d089f4bb8
